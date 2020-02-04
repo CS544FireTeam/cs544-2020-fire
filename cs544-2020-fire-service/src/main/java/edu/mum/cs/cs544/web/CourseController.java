db325d6ab23f874b7be72ea5456a807732ad4683
Yusuf Hussein <yusuf.hussein8894@gmail.com>
2020-02-04 02:30:30 -0600

    Add course restful controler

---
+++ cs544-2020-fire-service/src/main/java/edu/mum/cs/cs544/web/CourseController.java
@@ -0,0 +1,40 @@
+package edu.mum.cs.cs544.web;
+
+import edu.mum.cs.cs544.model.Course;
+import edu.mum.cs.cs544.service.course.ICourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private ICourseService courseService;
+
+    @GetMapping(value = "/courses")
+    public List<Course> getAllCourse(){
+        return this.courseService.findAll();
+    }
+
+    @GetMapping(value = "/courses/{id}")
+    public Course getCourse(@PathVariable Integer id){
+        return this.courseService.findById(id);
+    }
+
+    @PostMapping(value = "/courses")
+    public Course saveCourse(Course course){
+        return this.courseService.save(course);
+    }
+
+    @PutMapping(value = "/courses/{id}")
+    public Course updateCourse(Course course){
+        return this.courseService.save(course);
+    }
+
+    @DeleteMapping(value = "/courses/{id}")
+    public boolean deleteCourse(@PathVariable Integer id){
+        return this.courseService.delete(id);
+    }
+}
