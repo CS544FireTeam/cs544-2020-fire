800fcbfa4f3f95bce3e1b6968f38525f9179227f
Yusuf Hussein <yusuf.hussein8894@gmail.com>
2020-02-04 01:51:04 -0600

    Add all args constructor for Student/User/Faculty

--- cs544-2020-fire-service/src/main/java/edu/mum/cs/cs544/model/Faculty.java
+++ cs544-2020-fire-service/src/main/java/edu/mum/cs/cs544/model/Faculty.java
@@ -5,4 +5,15 @@ import javax.persistence.Entity;
 @Entity
 public class Faculty extends User {
 
+    public Faculty() {
+        super();
+    }
+
+    public Faculty(String username, String password, String fname, String lname) {
+        this.username = username;
+        this.password = password;
+        this.firstName = fname;
+        this.lastname = lname;
+        this.role = UserRole.FACULTY;
+    }
 }
