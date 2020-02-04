1e479fe4d6d369dfb6df88b95ac7f469ad64cff2
Mac <mac@Macs-MacBook-Pro.local>
2020-02-04 12:02:57 -0600

    added course edit

---
+++ cs544-2020-fire-frontend/src/app/modules/course/course-edit/course-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fire-course-edit',
+  templateUrl: './course-edit.component.html',
+  styleUrls: ['./course-edit.component.less']
+})
+export class CourseEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
