b92b6376451a5e62fedb85cc517b0142ed71aa35
YusufHussein <yusuf.hussein8894@gmail.com>
2020-02-05 15:45:31 -0600

    Revert "fix validation timeslot"

--- cs544-2020-fire-service/src/main/java/edu/mum/cs/cs544/security/SecurityConfig.java
+++ cs544-2020-fire-service/src/main/java/edu/mum/cs/cs544/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -22,7 +21,6 @@ import edu.mum.cs.cs544.dao.UserRepository;
 
 @Configuration
 @EnableWebSecurity
-@ConditionalOnProperty(name = "app.security", havingValue = "true")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private UserPrincipleDetailService userPrincipleDetailsService;
